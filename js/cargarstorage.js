7e170ed6da5099eac4abee24dcd5dc32c8b2ac6d
dany94xd <danimaxi60@hotmail.com>
2018-12-06 11:52:14 -0500

    areglado1.1

--- js/cargarstorage.js
+++ js/cargarstorage.js
@@ -1,12 +1,14 @@
-   var arrayAvaters=JSON.parse(personajes);
-     var imagenese=JSON.parse(imagenes);
+   
 
 // localStorage.setItem("personajes",JSON.stringify(arrayAvaters));
 // localStorage.setItem("imagenes",JSON.stringify(imagenese));
-
-if(localStorage.getItem('arrayAvaters') == null && localStorage.getItem('imagenese')==null){
+var ver=localStorage.getItem('personajes');
+if(ver == null ){
    
-    localStorage.setItem('arrayAvaters', JSON.stringify(arrayAvaters));
+     var arrayAvaters=JSON.parse(personajes);
+     var imagenese=JSON.parse(imagenes);
+
+    localStorage.setItem('personajes', JSON.stringify(arrayAvaters));
     localStorage.setItem('imagenese',JSON.stringify(imagenese));
     //Guardar el objeto en localStorage para tener acceso
 }
